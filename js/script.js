0035c85b68b959ad8883eaf3f505ba36ca7c706c
GromovBoris <drunkgonzo@gmail.com>
2025-04-18 20:57:07 +0300

    feat: slider base styles add

--- js/script.js
+++ js/script.js
@@ -11,4 +11,33 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   window.addEventListener("scroll", headerScroll);
+  const prevButton = document.querySelector(".prev");
+  const nextButton = document.querySelector(".next");
+  const slides = document.querySelectorAll(".slider-img");
+  let currentSlide = 0;
+
+  // Функция для показа текущего слайда
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.classList.remove("active");
+      if (i === index) {
+        slide.classList.add("active");
+      }
+    });
+  }
+
+  // Обработчик события для кнопки "вперед"
+  nextButton.addEventListener("click", () => {
+    currentSlide = (currentSlide + 1) % slides.length; // переходим к следующему слайду
+    showSlide(currentSlide);
+  });
+
+  // Обработчик события для кнопки "назад"
+  prevButton.addEventListener("click", () => {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length; // переходим к предыдущему слайду
+    showSlide(currentSlide);
+  });
+
+  // Инициализация первого слайда
+  showSlide(currentSlide);
 });
