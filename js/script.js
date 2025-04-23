6f310144eb88b8f3812b20bada7260bbec01a78b
GromovBoris <drunkgonzo@gmail.com>
2025-04-24 00:12:04 +0300

    feat: adaptive p1 add

--- js/script.js
+++ js/script.js
@@ -1,14 +1,64 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // HEADER HEIGHT&MARGIN
+
   const header = document.querySelector(".header");
   const headerHeight = header.offsetHeight;
+  const logo = document.querySelector(".logo");
+  const nav = document.querySelector(".nav");
 
   header.style.marginTop = `-${headerHeight}px`;
 
+  function getMargin() {
+    if (window.innerWidth > 800) {
+      const navWidth = nav.offsetWidth;
+      logo.style.marginRight = `${navWidth}px`;
+    }
+  }
+
+  function resetMargin() {
+    logo.style.marginRight = "";
+  }
+
   function headerScroll() {
     window.pageYOffset > headerHeight
-      ? header.classList.add("sticky")
-      : header.classList.remove("sticky");
+      ? (header.classList.add("sticky"), getMargin())
+      : (header.classList.remove("sticky"), resetMargin());
   }
+
+  function headerResize() {
+    if (header.classList.contains("sticky")) {
+      getMargin();
+    }
+  }
+
+  window.addEventListener("scroll", headerScroll);
+  window.addEventListener("resize", headerResize);
+
+  // IMG-CARGO HEIGHT
+
+  window.onload = function () {
+    setImgCargoHeight();
+  };
+
+  window.onresize = function () {
+    setImgCargoHeight();
+  };
+
+  function setImgCargoHeight() {
+    const supply = document.querySelector(".supply");
+    const imgCargo = document.querySelector(".img-cargo");
+
+    const supplyHeight = supply.offsetHeight;
+    const computedStyle = getComputedStyle(supply);
+    const marginTop = parseInt(computedStyle.marginTop);
+    const marginBottom = parseInt(computedStyle.marginBottom);
+
+    const totalHeight = supplyHeight + marginTop + marginBottom;
+
+    imgCargo.style.height = totalHeight + "px";
+  }
+
+  // SLIDER
 
   window.addEventListener("scroll", headerScroll);
   const prevButton = document.querySelector(".prev");
@@ -16,7 +66,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const slides = document.querySelectorAll(".slider-img");
   let currentSlide = 0;
 
-  // Функция для показа текущего слайда
   function showSlide(index) {
     slides.forEach((slide, i) => {
       slide.classList.remove("active");
@@ -26,18 +75,15 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  // Обработчик события для кнопки "вперед"
   nextButton.addEventListener("click", () => {
-    currentSlide = (currentSlide + 1) % slides.length; // переходим к следующему слайду
+    currentSlide = (currentSlide + 1) % slides.length;
     showSlide(currentSlide);
   });
 
-  // Обработчик события для кнопки "назад"
   prevButton.addEventListener("click", () => {
-    currentSlide = (currentSlide - 1 + slides.length) % slides.length; // переходим к предыдущему слайду
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
     showSlide(currentSlide);
   });
 
-  // Инициализация первого слайда
   showSlide(currentSlide);
 });
